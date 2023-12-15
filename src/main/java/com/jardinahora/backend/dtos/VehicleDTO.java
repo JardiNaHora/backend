20db2691e4241e7abb7868b7850a4ccdd1f6f1b0
LeoVPires <leonardovasconcelos73@gmail.com>
2023-12-15 19:46:20 -0300

    feat(vehicle_endpoint): changed model to match front end page

--- src/main/java/com/jardinahora/backend/dtos/VehicleDTO.java
+++ src/main/java/com/jardinahora/backend/dtos/VehicleDTO.java
@@ -1,6 +1,7 @@
 package com.jardinahora.backend.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record VehicleDTO(@NotBlank String type) {
+public record VehicleDTO(@NotBlank String type, @NotBlank String name, @NotBlank String plate, @NotNull Integer passengers ) {
 }
